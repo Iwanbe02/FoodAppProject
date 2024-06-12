f8629c07106d1e977d86e8f1d00a44dbb330386e
Châu Thiên Ân <chauthienan01072002@gmail.com>
2024-06-12 13:52:52 +0700

    config firebase

---
+++ app/src/main/java/com/example/foodappproject/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.example.foodappproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+
+    }
+}
