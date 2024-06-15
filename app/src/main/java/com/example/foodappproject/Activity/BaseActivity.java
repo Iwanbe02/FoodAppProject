b7f4e81881b97b8a88bde8de7a535b2d4fa56f6b
Châu Thiên Ân <chauthienan01072002@gmail.com>
2024-06-15 18:56:11 +0700

    Init banner

--- app/src/main/java/com/example/foodappproject/Activity/BaseActivity.java
+++ app/src/main/java/com/example/foodappproject/Activity/BaseActivity.java
@@ -11,14 +11,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class BaseActivity extends AppCompatActivity {
-    FirebaseAuth mAuth;
+    //FirebaseAuth mAuth;
     FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        database = FirebaseDatabase.getInstance();
-        mAuth = FirebaseAuth.getInstance();
-        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+        database = FirebaseDatabase.getInstance("https://foodapp-73733-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().getDatabase();
+        //mAuth = FirebaseAuth.getInstance();
+        //getWindow().setStatusBarColor(getResources().getColor(R.color.white));
         Window w=getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
--- app/src/main/java/com/example/foodappproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/foodappproject/Activity/MainActivity.java
@@ -5,9 +5,15 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 
 import com.example.foodappproject.Adapter.CategoryAdapter;
+import com.example.foodappproject.Adapter.SliderAdapter;
 import com.example.foodappproject.Domain.Category;
+import com.example.foodappproject.Domain.SliderItems;
+import com.example.foodappproject.R;
 import com.example.foodappproject.databinding.ActivityMainBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,8 +31,48 @@ public class MainActivity extends BaseActivity {
         setContentView(binding.getRoot());
 
         initCategory();
+        initBanner();
+        setVariable();
     }
 
+    private void initBanner() {
+        DatabaseReference myRef = database.getReference("Banners");
+        binding.progressBarBanner.setVisibility(View.VISIBLE);
+        ArrayList<SliderItems> items = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue : snapshot.getChildren()){
+                        items.add(issue.getValue(SliderItems.class));
+                    }
+                    banner(items);
+                    binding.progressBarBanner.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void banner(ArrayList<SliderItems> items){
+        binding.viewpager2.setAdapter(new SliderAdapter(items, binding.viewpager2));
+        binding.viewpager2.setClipChildren(false);
+        binding.viewpager2.setClipToPadding(false);
+        binding.viewpager2.setOffscreenPageLimit(3);
+        binding.viewpager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+
+        binding.viewpager2.setPageTransformer(compositePageTransformer);
+    }
+    private void setVariable(){
+        binding.bottomMenu.setItemSelected(R.id.home, true);
+    }
     private void initCategory() {
         DatabaseReference myRef=database.getReference("Category");
         binding.progressBarCategory.setVisibility(View.VISIBLE);
